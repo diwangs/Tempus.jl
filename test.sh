f4c087fba3181c0f6026cf6d93534f0d0f60c289
diwangs <diwangs@cs.jhu.edu>
2023-02-06 10:30:38 -0500

    [Paper] Completed section 2

---
+++ test.sh
@@ -0,0 +1,20 @@
+julia --project=. src/Tempus.jl fattree4gen.json conv
+julia --project=. src/Tempus.jl fattree6gen.json conv
+julia --project=. src/Tempus.jl fattree8gen.json conv
+julia --project=. src/Tempus.jl fattree10gen.json conv
+julia --project=. src/Tempus.jl fattree12gen.json conv
+julia --project=. src/Tempus.jl fattree14gen.json conv
+
+julia --project=. src/Tempus.jl fattree4gen.json cons
+julia --project=. src/Tempus.jl fattree6gen.json cons
+julia --project=. src/Tempus.jl fattree8gen.json cons
+julia --project=. src/Tempus.jl fattree10gen.json cons
+julia --project=. src/Tempus.jl fattree12gen.json cons
+julia --project=. src/Tempus.jl fattree14gen.json cons
+
+julia --project=. src/Tempus.jl fattree4gen.json base
+julia --project=. src/Tempus.jl fattree6gen.json base
+julia --project=. src/Tempus.jl fattree8gen.json base
+julia --project=. src/Tempus.jl fattree10gen.json base
+julia --project=. src/Tempus.jl fattree12gen.json base
+julia --project=. src/Tempus.jl fattree14gen.json base
